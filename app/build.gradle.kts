42ba1ed3cd39b4ff9d7cfb25ef6eaaab4cb5f285
Emre Esen <emre@Emres-MacBook-Pro.local>
2024-09-09 22:41:36 +0300

    [DEV][firebase implementation is completed]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
     id("androidx.navigation.safeargs.kotlin")
     id("com.google.dagger.hilt.android")
+    id("com.google.firebase.crashlytics")
 }
 
 apply("dependencies.gradle")
@@ -31,6 +33,7 @@ android {
     val releaseVersionUrl = "https://api.github.com/repos/emreesen27/Hyper-N/releases/latest"
     buildTypes {
         getByName("debug") {
+            applicationIdSuffix = ".debug"
             buildConfigField("String", "MY_CHANNEL", "\"$myChannelValue\"")
             buildConfigField("String", "RELEASE_VERSION", "\"$releaseVersionUrl\"")
         }
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,6 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
     id("androidx.navigation.safeargs") version "2.5.0" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.firebase.crashlytics") version "2.9.9" apply false
 }
